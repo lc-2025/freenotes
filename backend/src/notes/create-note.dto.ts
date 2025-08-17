906b10e70ea6dc65470c3af1b432a256d527693a
the-speaker3 <the_speaker3@protonmail.com>
2025-08-17 14:35:46 +0200

    docs: Note DTO implementation

--- backend/src/notes/create-note.dto.ts
+++ backend/src/notes/create-note.dto.ts
@@ -2,6 +2,14 @@ import { Tag } from 'src/tags/schemas/tag.schema';
 import { User } from 'src/users/schemas/user.schema';
 import { INote } from './types/note.types';
 
+/**
+ * @description Note Data Transfer Object class
+ * Defines the note request payload
+ * @author Luca Cattide
+ * @date 17/08/2025
+ * @class CreateNoteDto
+ * @implements {INote}
+ */
 class CreateNoteDto implements INote {
   title: string;
   body: string;
