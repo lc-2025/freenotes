78ca73772275af6635f8cffc5f9ec424ec9c5112
the-speaker3 <the_speaker3@protonmail.com>
2025-08-18 10:56:10 +0200

    docs: Note custom decorator update

--- backend/src/notes/notes.decorator.ts
+++ backend/src/notes/notes.decorator.ts
@@ -1,6 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { TNoteDecorator } from './types/note.types';
 
+/**
+ * Note custom decorator
+ * Manages `param` and `body` properties
+ * together
+ */
 const Note = createParamDecorator(
   (data: TNoteDecorator, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
